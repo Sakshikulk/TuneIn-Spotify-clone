2efdc9d36e8dc3cd23d91cdd7ea41a739e9e9819
Sakshikulk <107936222+Sakshikulk@users.noreply.github.com>
2023-09-04 17:46:55 +0530

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("loginBtn");
+let signIncontainer = document.getElementById("Signinform");
+let logIncontainer = document.getElementById("Loginform");
+
+
+function hide(){
+    signIncontainer.style.display = "none";
+    Loginform.style.display = "block";
+}
